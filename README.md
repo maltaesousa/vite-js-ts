45a2ad47b70d57702e32e2e1a817cdaee9c55cd9
maltaesousa <stephane.maltaesousa@ne.ch>
2023-07-07 11:06:24 +0200

    vite init vanillajs

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vite combining JavaScript and TypeScript
+
+## How does this repo work ?
+
+This repo has several commits showing what steps are necessary to include Typescript into a vanillaJS app bundled with vite.
+You can checkout to the commit you want to and run the app with `vite` command.
+
+## Run the app
+
+```bash
+npm install
+vite
+```
---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css'
+import viteLogo from '/vite.svg'
+import { setupCounterJS } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter-js" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounterJS(document.querySelector('#counter-js'))
